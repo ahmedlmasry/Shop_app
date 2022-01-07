192164ca9c4f4dfe3939da75103dce8495ec982d
ahmedlmasry <elmasryahmed162@gmail.com>
2022-01-08 00:11:08 +0200

    initial commit

---
+++ lib/modules/splash-screen.dart
@@ -0,0 +1,22 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/main.dart';
+import 'package:shop_app/modules/on%20boarding/on_boarding.dart';
+import 'package:shop_app/shared/components/components.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EasySplashScreen(
+        logo: const Image(image: AssetImage('assets/images/shop_logo.jpg')),
+        showLoader: false,
+        title: const Text('Salla Store'),
+        durationInSeconds: 3,
+        navigator: OnBoardingScreen(),
+      ),
+    );
+  }
+}
